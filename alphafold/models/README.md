ca5c26d40cc49c4c251bfb22f06ec41e489cee07
GuanyiLi-Craig <bitforce.studio@gmail.com>
2021-01-30 19:54:37 +0000

    add more

---
+++ alphafold/models/README.md
@@ -0,0 +1,1 @@
+# AlphaFold Models
--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -p 8888:8888 --gpus all --ipc=host -v /media/data:/alphafold alphafold_pipeline jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token= --notebook-dir='/data'
+sudo docker run -d -p 8888:8888 --gpus all --ipc=host -v <path-to-directory>/alphafold:/data alphafold_pipeline jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token= --notebook-dir='/data'
